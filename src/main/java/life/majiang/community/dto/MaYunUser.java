acadf7181e890523ce7618b4f9df62a8c9f81cb5
Beardz <Beardz@github.com>
2020-11-18 13:29:02 +0800

    init repo

---
+++ src/main/java/life/majiang/community/dto/MaYunUser.java
@@ -0,0 +1,12 @@
+package life.majiang.community.dto;
+
+import lombok.Data;
+
+@Data
+public class MaYunUser {
+    private Long id;
+    private String name;
+    private String avatarUrl;
+    private String bio;
+
+}
